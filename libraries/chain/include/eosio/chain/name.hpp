98bce97fb78eda6e4432a4b464285b8fc07b07a6
bytemaster <bytemaster@gmx.com>
2019-10-10 17:19:37 +0800

    update name

--- libraries/chain/include/eosio/chain/name.hpp
+++ libraries/chain/include/eosio/chain/name.hpp
@@ -6,41 +6,75 @@
 namespace eosio { namespace chain {
    using std::string;
 
-   static constexpr uint64_t char_to_symbol( char c ) {
+   // static constexpr uint64_t char_to_symbol( char c ) {
+   //    if( c >= 'a' && c <= 'z' )
+   //       return (c - 'a') + 6;
+   //    if( c >= '1' && c <= '5' )
+   //       return (c - '1') + 1;
+   //    return 0;
+   // }
+
+   static constexpr uint128_t char_to_symbol( char c ) {
       if( c >= 'a' && c <= 'z' )
-         return (c - 'a') + 6;
-      if( c >= '1' && c <= '5' )
-         return (c - '1') + 1;
+         return (c - 'a') + 10;
+      if( c >= '0' && c <= '9' )
+         return c - '0';
+      if( c == '_' )
+         return 36;
+      if( c == '@' )
+         return 37;
+      if( c == '.' )
+         return 38;
       return 0;
    }
 
-   // Each char of the string is encoded into 5-bit chunk and left-shifted
-   // to its 5-bit slot starting with the highest slot for the first char.
-   // The 13th char, if str is long enough, is encoded into 4-bit chunk
-   // and placed in the lowest 4 bits. 64 = 12 * 5 + 4
-   static constexpr uint64_t string_to_name( const char* str )
+   // // Each char of the string is encoded into 5-bit chunk and left-shifted
+   // // to its 5-bit slot starting with the highest slot for the first char.
+   // // The 13th char, if str is long enough, is encoded into 4-bit chunk
+   // // and placed in the lowest 4 bits. 64 = 12 * 5 + 4
+   // static constexpr uint64_t string_to_name( const char* str )
+   // {
+   //    uint64_t name = 0;
+   //    int i = 0;
+   //    for ( ; str[i] && i < 12; ++i) {
+   //        // NOTE: char_to_symbol() returns char type, and without this explicit
+   //        // expansion to uint64 type, the compilation fails at the point of usage
+   //        // of string_to_name(), where the usage requires constant (compile time) expression.
+   //         name |= (char_to_symbol(str[i]) & 0x1f) << (64 - 5 * (i + 1));
+   //     }
+
+   //    // The for-loop encoded up to 60 high bits into uint64 'name' variable,
+   //    // if (strlen(str) > 12) then encode str[12] into the low (remaining)
+   //    // 4 bits of 'name'
+   //    if (i == 12)
+   //        name |= char_to_symbol(str[12]) & 0x0F;
+   //    return name;
+   // }
+
+   // Each char of the string is encoded into 6-bit chunk and left-shifted
+   // to its 6-bit slot starting with the highest slot for the first char.
+   // 128 = 21 * 6 + 2
+   static constexpr uint128_t string_to_name( const char* str )
    {
-      uint64_t name = 0;
+      uint128_t name = 0;
       int i = 0;
-      for ( ; str[i] && i < 12; ++i) {
+      for ( ; str[i] && i < 21; ++i) {
           // NOTE: char_to_symbol() returns char type, and without this explicit
-          // expansion to uint64 type, the compilation fails at the point of usage
+          // expansion to uint128 type, the compilation fails at the point of usage
           // of string_to_name(), where the usage requires constant (compile time) expression.
-           name |= (char_to_symbol(str[i]) & 0x1f) << (64 - 5 * (i + 1));
+           name |= (char_to_symbol(str[i]) & 0x3f) << (128 - 6 * (i + 1));
        }
 
-      // The for-loop encoded up to 60 high bits into uint64 'name' variable,
-      // if (strlen(str) > 12) then encode str[12] into the low (remaining)
-      // 4 bits of 'name'
-      if (i == 12)
-          name |= char_to_symbol(str[12]) & 0x0F;
+      // The for-loop encoded up to 126 high bits into uint128 'name' variable,
+      // the last 2 bits are fill with 1.
+      name |= 0x03;
       return name;
    }
 
 #define N(X) eosio::chain::string_to_name(#X)
 
    struct name {
-      uint64_t value = 0;
+      uint128_t value = 0;
       bool empty()const { return 0 == value; }
       bool good()const  { return !empty();   }
 
@@ -57,7 +91,7 @@ namespace eosio { namespace chain {
 
       string to_string() const { return string(*this); }
 
-      name& operator=( uint64_t v ) {
+      name& operator=( uint128_t v ) {
          value = v;
          return *this;
       }
@@ -81,25 +115,25 @@ namespace eosio { namespace chain {
       friend bool operator >=( const name& a, const name& b ) { return a.value >= b.value; }
       friend bool operator == ( const name& a, const name& b ) { return a.value == b.value; }
 
-      friend bool operator == ( const name& a, uint64_t b ) { return a.value == b; }
-      friend bool operator != ( const name& a, uint64_t b ) { return a.value != b; }
+      friend bool operator == ( const name& a, uint128_t b ) { return a.value == b; }
+      friend bool operator != ( const name& a, uint128_t b ) { return a.value != b; }
 
       friend bool operator != ( const name& a, const name& b ) { return a.value != b.value; }
 
       operator bool()const            { return value; }
-      operator uint64_t()const        { return value; }
+      operator uint128_t()const        { return value; }
       operator unsigned __int128()const       { return value; }
    };
 
 } } // eosio::chain
 
 namespace std {
-   template<> struct hash<eosio::chain::name> : private hash<uint64_t> {
+   template<> struct hash<eosio::chain::name> : private hash<uint128_t> {
       typedef eosio::chain::name argument_type;
-      typedef typename hash<uint64_t>::result_type result_type;
+      typedef typename hash<uint128_t>::result_type result_type;
       result_type operator()(const argument_type& name) const noexcept
       {
-         return hash<uint64_t>::operator()(name.value);
+         return hash<uint128_t>::operator()(name.value);
       }
    };
 };
--- libraries/chain/name.cpp
+++ libraries/chain/name.cpp
@@ -7,8 +7,8 @@
 namespace eosio { namespace chain { 
 
    void name::set( const char* str ) {
-      const auto len = strnlen(str, 14);
-      EOS_ASSERT(len <= 13, name_type_exception, "Name is longer than 13 characters (${name}) ", ("name", string(str)));
+      const auto len = strnlen(str, 22);
+      EOS_ASSERT(len <= 21, name_type_exception, "Name is longer than 21 characters (${name}) ", ("name", string(str)));
       value = string_to_name(str);
       EOS_ASSERT(to_string() == string(str), name_type_exception,
                  "Name not properly normalized (name: ${name}, normalized: ${normalized}) ",
@@ -16,16 +16,34 @@ namespace eosio { namespace chain {
    }
 
    // keep in sync with name::to_string() in contract definition for name
-   name::operator string()const {
-     static const char* charmap = ".12345abcdefghijklmnopqrstuvwxyz";
+   // name::operator string()const {
+   //   static const char* charmap = ".12345abcdefghijklmnopqrstuvwxyz";
 
-      string str(13,'.');
+   //    string str(13,'.');
+
+   //    uint64_t tmp = value;
+   //    for( uint32_t i = 0; i <= 12; ++i ) {
+   //       char c = charmap[tmp & (i == 0 ? 0x0f : 0x1f)];
+   //       str[12-i] = c;
+   //       tmp >>= (i == 0 ? 4 : 5);
+   //    }
+
+   //    boost::algorithm::trim_right_if( str, []( char c ){ return c == '.'; } );
+   //    return str;
+   // }
+
+   name::operator string()const {
+     static const char* charmap = "0123456789abcdefghijklmnopqrstuvwxyz_@.";
+
+      string str(21,'.');
 
       uint64_t tmp = value;
-      for( uint32_t i = 0; i <= 12; ++i ) {
-         char c = charmap[tmp & (i == 0 ? 0x0f : 0x1f)];
-         str[12-i] = c;
-         tmp >>= (i == 0 ? 4 : 5);
+      for( uint32_t i = 0; i <= 21; ++i ) {
+         if (i > 0) {
+            char c = charmap[tmp & 0x3f];
+            str[21-i] = c;
+         }
+         tmp >>= (i == 0 ? 2 : 6);
       }
 
       boost::algorithm::trim_right_if( str, []( char c ){ return c == '.'; } );
